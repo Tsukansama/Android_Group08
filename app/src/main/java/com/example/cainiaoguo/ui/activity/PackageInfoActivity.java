011d1f2145f6e5dc9ea6403224bc67c8f8c354a4
anyuan <380666201@qq.com>
2020-05-09 15:12:10 +0800

    安源5月9日15:11提交

--- app/src/main/java/com/example/cainiaoguo/ui/activity/PackageInfoActivity.java
+++ app/src/main/java/com/example/cainiaoguo/ui/activity/PackageInfoActivity.java
@@ -21,23 +21,8 @@ public class PackageInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_package_info);
 
-        ListView addrListView = (ListView) findViewById(R.id.time_list_view);
 
-        List<Map<String, ?>> dataList = new ArrayList<>();
 
-        for (int i = 0; i < 50; i++) {
-            Map<String, String> map = new HashMap<>();
-
-            map.put("addr", "北京市");
-
-            dataList.add(map);//将列表的内容添加进listview中，但不一定都显示
-        }
-
-        String[] from = {"addr"};//自定义listview的item中需要显示的数据
-        int[] to = {R.id.title};
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, dataList, R.layout.item_time_line_listview, from, to);
-
-        addrListView.setAdapter(simpleAdapter);
     }
 
 }
